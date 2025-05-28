8c4b01132b3a87fee9357d450fa3ee8f92fc6fd0
Korir Carlos <carlkorir226@gmail.com>
2025-05-28 16:05:59 +0300

    adds the tour package operations in helpers and cli

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 from lib.connection import init_db
 from helpers import (exit_program,list_customers, find_customer_by_name,find_customer_by_id,
-    create_customer,update_customer_details, delete_customer_with_details,)
+    create_customer,update_customer_details, delete_customer_with_details, list_tour_packages, find_tour_package_by_name,
+    find_tour_package_by_id, create_tour_package, update_tour_package)
 
 def main_menu():
     while True:
@@ -51,7 +52,30 @@ def customer_menu():
         elif choice == "7":
             break
 
-            
+def tour_package_menu():
+    while True:
+        print("\nTour Package Operations")
+        print("1. List tour packages")
+        print("2. Find Tour Package By Name")
+        print("3. Find Tour Package By ID")
+        print("4. Create Tour Package")
+        print("5. Update Tour Package")
+        print("6. Delete Tour Package")
+        print("7. Back")
+        choice = input("Select an option (1-7): ")
 
+        if choice == "1":
+            list_tour_packages()
+        elif choice == "2":
+            find_tour_package_by_name()
+        elif choice == "3":
+            find_tour_package_by_id()
+        elif choice == "4":
+            create_tour_package()
+        elif choice == "5":
+            update_tour_package()
+        elif choice == "7":
+            break
+        pass
 if __name__ == "__main__":
     main_menu()
--- helpers.py
+++ helpers.py
@@ -1,9 +1,14 @@
 from lib.models.customer import Customer
 from lib.models.tour_package import TourPackage
 from lib.models.payment import Payment
-from lib.connection import Session  
+from lib.connection import Session 
+from sqlalchemy.orm import sessionmaker 
+from lib.connection import engine
 from datetime import datetime
 import re
+
+Session = sessionmaker(bind=engine)
+
 
 def exit_program():
     print("Thanks and Goodbye! ")
@@ -65,3 +70,53 @@ def delete_customer_with_details():
         print(f"Customer {id_} successfully removed from db")
     else:
         print(f"Customer{id_} not found")
+
+def list_tour_packages():
+    tour_packages = TourPackage.get_all()
+    for tour_package in tour_packages:
+        print(tour_package)
+
+def find_tour_package_by_name():
+    name = input("Enter tour package's name: ")
+    tour_package = next((tp for tp in TourPackage.get_all()
+    if tp.name.lower()==name.lower()), None)
+    print(tour_package) if tour_package else print(f"tour package {name} not found in the database")
+def find_tour_package_by_id():
+    id_ = input("Enter the tour package's ID: ")
+    tour_package = TourPackage.find_by_id(id_)
+    print(tour_package) if tour_package else print("tour package {id_} not found in the db")
+def create_tour_package():
+    name = input("Enter the tour package name: ")
+    price = float(input("Enter the tour package price: "))
+    departure_date = input("Enter the tour package's depature date (YYYY-MM-DD): ")
+    slots_remaining = int(input("Enter the remaining slots for the tour package: "))
+    session = Session()
+    try:
+        tour_package = TourPackage(
+            name=name,
+            price=price,
+            departure_date=departure_date,
+            slots_remaining=slots_remaining
+        )
+        session.add(tour_package)
+        session.commit()
+        print(f"Tour package '{tour_package.name}' successfully added to db with ID {tour_package.id}")
+    except Exception as e:
+        print("error creating tour package", e)
+def update_tour_package():
+    id_ = input("Enter tour package id to be updated: ")
+    session = Session()
+    if tour_package := TourPackage.find_by_id(id_):
+        try:
+            name = input("Enter tourpackage's new name: ")
+            tour_package.name = name
+            price = float(input("Enter tourpackage's new price: "))
+            tour_package.price = price
+            departure_date = input("Enter the tour package's new departure date (YYYY-MM-DD): ")
+            tour_package.departure_date = departure_date
+            slots_remaining = int(input("Enter the tour package's new slots remaining: "))
+            tour_package.slots_remaining = slots_remaining
+            session.add(tour_package)
+            session.commit()
+        except Exception as e:
+            print("Error updating tour package: ", e)
